7f6be1cf58e065d47dbc343f9997500bf2550f25
shifa4532 <shi567fa@gmail.com>
2026-02-09 22:00:35 +0530

    edit 3.1

--- sql-practise.sql
+++ sql-practise.sql
@@ -1,4 +1,17 @@
 create database college;
 use college;
-drop table student;
-create table teacher;
+create table teacher(
+   id int primary key,
+   name varchar(30),
+   subject varchar(15),
+   salary int
+);
+insert into teacher 
+(id, name, subject, salary)
+values 
+(23, "ajay", "math", 50000),
+(47, "bharat", "english", 60000),
+(18, "chetan", "chemistry", 45000),
+(19, "divya", "physics", 75000);
+
+select * from teacher;
