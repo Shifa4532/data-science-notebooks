6ff25a74541814c4fd7ba0b387801ff95f79a932
shifa4532 <shi567fa@gmail.com>
2026-02-09 21:42:34 +0530

    edit 3.1

--- sql-practise.sql
+++ sql-practise.sql
@@ -1,14 +1,4 @@
 create database college;
-create database xyz_company;
-drop database xyz_company;
 use college;
-create table student(
-   rollno int,
-   name varchar(30),
-   age int
-);
-insert into student 
-values
-(101, "adam", 39),
-(102, "john", 40);
-select * from student;
+drop table student;
+create table teacher;
