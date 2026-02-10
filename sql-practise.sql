a7e20a8ba5092df50af9fff2e4359ca1b10d8292
shifa4532 <shi567fa@gmail.com>
2026-02-10 21:03:47 +0530

    edit 4

--- sql-practise.sql
+++ sql-practise.sql
@@ -15,3 +15,13 @@ values
 (19, "divya", "physics", 75000);
 
 select * from teacher;
+
+select * from teacher
+where salary > 55000;
+
+alter table teacher
+change column salary ctc int;
+set sql_safe_updates = 0;
+update teacher
+set ctc = ctc + ctc * 0.25
+
